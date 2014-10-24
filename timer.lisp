0c7867676411195c0d016649cd41eb4c6ce40653
Francois-Rene Rideau <tunes@google.com>
2014-10-24 18:32:24 -0400

    Update this program with exercises from "The Scientific 7-Minute Workout" by Gretchen Reynolds. Also, use samples from FreeSound.org, all converted to 44100 Hz to make mixalot happier. Finally, improve the output and provide a #!/usr/bin/cl script.

--- timer.lisp
+++ timer.lisp
@@ -3,7 +3,7 @@
 (in-package :workout-timer)
 
 (defun oggfile (x)
-  (namestring (asdf:system-relative-pathname :workout-timer (asdf:strcat x ".ogg"))))
+  (namestring (asdf:system-relative-pathname :workout-timer x :type "ogg")))
 
 (defun oggstream (x)
   (make-vorbis-streamer (oggfile x) :output-rate 44100))
@@ -11,15 +11,16 @@
 (defun addogg (x)
   (mixer-add-streamer *mixer* (oggstream x)))
 
+;; recoded with sox ${in} -r 44100 ${out}.ogg
 (defun bell ()
-  (addogg "bell"))
+  (addogg "bell")) ;; from https://www.freesound.org/people/Taira%20Komori/sounds/212057/
 (defun click ()
-  (addogg "click"))
+  (addogg "click")) ;; from https://www.freesound.org/people/EdgardEdition/sounds/113634/
 (defun buzzer ()
-  (addogg "buzzer"))
+  (addogg "buzzer")) ;; from https://www.freesound.org/people/guitarguy1985/sounds/54047/
 (defun gong ()
-  (addogg "gong"))
-  
+  (addogg "gong")) ;; from https://www.freesound.org/people/Veiler/sounds/207168/
+
 
 (defun integer-digits (n &optional (base 10))
   "Given a non-negative integer N, return how many digits it takes to write n in given BASE"
@@ -30,7 +31,7 @@
     (when (or (null n)
               (null *current-time*)
               (when (timestamp< current-time *current-time*)
-                (format *error-output* "~&Your clock ran backwards. Adjusting.~%")
+                (format! *error-output* "~&Your clock ran backwards. Adjusting.~%")
                 t))
       (setf *current-time* current-time))
     (when (null n)
@@ -44,29 +45,41 @@
          (setf *current-time* target-time))
         (t
          ;; already missed the deadline, skipping.
-         (format *error-output* "~&Oops, skipped a beat~%")
+         ;;(format! *error-output* "~&Oops, skipped a beat~%")
          (setf *current-time* current-time))))))
 
 (defun countdown (n &key click)
   (loop :with l = (integer-digits n)
         :for i :from n :downto 0 :do
-    (format t "  ~v,' D~C" l i #\return)
+    (format! t "  ~v,' D~C" l i #\return)
     (unless (zerop i)
       (when click (click))
       (vsleep 1))))
 
-(defun my-workout ()
+;; See exercises here:
+;; http://well.blogs.nytimes.com/2013/05/09/the-scientific-7-minute-workout/
+;; http://graphics8.nytimes.com/images/2013/05/12/health/12well_physed/12well_physed-tmagArticle.jpg
+;; https://www.youtube.com/watch?v=ECxYJcnvyMw
+(defun my-workout (&key
+                     (work-seconds 30)
+                     (pause-seconds 10)
+                     (exercises
+                      '("Jumping jack" "Wall sit" "Push-up" "Abdominal crunch"
+                        "Step-up onto chair" "Squat" "Triceps dip on chair" "Plank"
+                        "High knees running in place" "Lunge" "Push-up and rotation" "Side plank")))
   (vsleep 0)
-  (loop :for i :from 0 :do
+  (loop :for (exercise . morep) :on exercises :do
+    (format! t "~&~As!~%" exercise)
     (bell)
-    (countdown 25 :click t)
-    (buzzer)
-    (if (< i 4)
-      (vsleep 5)
-      (progn (gong) (return)))))
+    (countdown work-seconds :click t)
+    (cond
+      (morep (buzzer)
+             (format! t "~&Rest! (next: ~As)~%" (car morep))
+             (countdown pause-seconds :click nil))
+      (t (gong) (format! t "~&Done!") (return)))))
 
 (defun mix-it ()
-  (setf *mixer* (create-mixer))
+  (setf *mixer* (create-mixer :rate 44100))
   (my-workout)
   (destroy-mixer *mixer*)
   (setf *mixer* nil)
